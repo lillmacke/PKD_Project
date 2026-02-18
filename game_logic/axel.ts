aa9dfe8812d9ed2c87317888647106e5922b9faa
AxelByback <axel.neckman-byback.5646@student.uu.se>
2026-02-18 13:35:25 +0100

    tärning update

--- game_logic/axel.ts
+++ game_logic/axel.ts
@@ -3,7 +3,11 @@ function dice_roll(): Dice {
     let roll_2 = Math.floor(Math.random() * 6) + 1;
 
     if (roll_1 === roll_2) {
+        console.log("Double:", roll_1, roll_1, roll_1, roll_1);
         return { values: [roll_1, roll_1, roll_1, roll_1] };
     } 
+    console.log(roll_1, roll_2);
     return { values: [roll_1, roll_2] }; 
 }
+
+
