7a3c4039c49c29c974f27b7356548a359319c6ae
VLenninger <vilgot.lenninger.8242@student.uu.se>
2026-02-18 13:22:20 +0100

    Tar bort types

--- Backgammon bot/Simple_bot.ts
+++ Backgammon bot/Simple_bot.ts
@@ -1,34 +1,4 @@
-type Player = "white" | "black";
 
-/**
- * Triangles on the board
- * Invariant: count >= 0
- * if count === 0 -> owner === null
- */
-type Point = {
-    player: Player | null;
-    count: number;
-};
-
-/**
- * length is 2, 4 if doubles
- * Value between 1-6
- */
-type Dice = {
-    values: number[];
-};
-
-type Board = {
-    points: Point[]; //length 24
-    bar: Record<Player, number>;
-    borne_off: Record<Player, number>;
-}
-
-type GameState = {
-    board: Board;
-    current_player: Player;
-    dice: Dice | null; //if dice === null, dice not rolled yet 
-}
 
 
 function make_move(game: GameState) null {
