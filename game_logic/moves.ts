28ec4b99eb08412a84804de0ecafffbd81b57ae6
lillmacke <markus9karlsson@hotmail.com>
2026-02-18 14:40:57 +0100

    is_legal_move klar?

--- game_logic/moves.ts
+++ game_logic/moves.ts
@@ -4,11 +4,16 @@ import {Dice, GameState, Board, Player} from "./types";
 
 function is_legal_move(state: GameState, from: number, die: number): boolean {    
     if (state.current_player === "black" && 
-        state.board.points[from+die] === state.board.points.
-        
-    ) {
-        return false 
-        console.log("Invalid move");
-    }
-    return true;
+        state.board.points[from+die].player === "white" &&
+        state.board.points[from+die].count > 1) {
+            console.log("Invalid move");
+            return false;           
+    } else if 
+        (state.current_player === "white" && 
+        state.board.points[from+die].player === "black" &&
+        state.board.points[from+die].count > 1) {
+            console.log("Invalid move");
+            return false;
+   }
+   return true;
 };
