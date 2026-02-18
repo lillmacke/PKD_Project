d06789ee153dc2b0d122b9e4bb6a21d790b6011d
AxelByback <axel.neckman-byback.5646@student.uu.se>
2026-02-18 13:19:52 +0100

    tärning

---
+++ axel.ts
@@ -0,0 +1,9 @@
+function dice_roll(): Dice {
+    let roll_1 = Math.floor(Math.random() * 6) + 1;
+    let roll_2 = Math.floor(Math.random() * 6) + 1;
+
+    if (roll_1 === roll_2) {
+        return { values: [roll_1, roll_1, roll_1, roll_1] };
+    } 
+    return { values: [roll_1, roll_2] }; 
+}
