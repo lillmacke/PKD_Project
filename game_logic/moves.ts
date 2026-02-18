60632929922662192e2f25f790c78fb4732e02bd
lillmacke <markus9karlsson@hotmail.com>
2026-02-18 14:30:21 +0100

    asd

--- game_logic/moves.ts
+++ game_logic/moves.ts
@@ -1,8 +1,14 @@
 import {Dice, GameState, Board, Player} from "./types";
 
-function is_legal_move(state: GameState, from: number, die: number): boolean {
-    if (state.board.points[from]) {
+
+
+function is_legal_move(state: GameState, from: number, die: number): boolean {    
+    if (state.current_player === "black" && 
+        state.board.points[from+die] === state.board.points.
+        
+    ) {
         return false 
         console.log("Invalid move");
     }
+    return true;
 };
