588a912c9f9c13b5bbb1f95a9315f0f654433b1e
AxelByback <axel.neckman-byback.5646@student.uu.se>
2026-02-19 11:32:22 +0100

    test

--- game_logic/moves.ts
+++ game_logic/moves.ts
@@ -2,20 +2,25 @@ import {Dice, GameState, Board, Player} from "./types";
 
 
 
-function is_valid_move(state: GameState, from: number, die: number): boolean {    
-    if (state.current_player === "black" && 
-        state.board.points[from-die].player === "white" &&
-        state.board.points[from-die].count > 1) {
-            console.log("Invalid move");
-            return false;           
-    } else if 
-        (state.current_player === "white" && 
-        state.board.points[from+die].player === "black" &&
-        state.board.points[from+die].count > 1) {
-            console.log("Invalid move");
-            return false;
-   }
-   return true;
+function is_valid_move(state: GameState, from: number, die: number): boolean {   
+    if(from >= 0 && from < 24) {
+        if (from + die < 24 || from - die >= 0) {
+    
+            if (state.current_player === "black" && 
+                state.board.points[from-die].player === "white" &&
+                state.board.points[from-die].count > 1) {
+                    console.log("Invalid move");
+                    return false;           
+            } else if 
+                (state.current_player === "white" && 
+                state.board.points[from+die].player === "black" &&
+                state.board.points[from+die].count > 1) {
+                    console.log("Invalid move");
+                    return false;
+                }
+        }
+    }
+    return true;
 };
 
 function apply_move(state: GameState, from: number, die: number): GameState {
