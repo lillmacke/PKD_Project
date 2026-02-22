37ee49034de5b4a9f465a1302729cb573c6500af
VLenninger <vilgot.lenninger.8242@student.uu.se>
2026-02-22 19:04:40 +0100

    Byggt på bot kod, logiken bör fungera, syntaxfel finns garanterat.

--- Backgammon bot/Simple_bot.ts
+++ Backgammon bot/Simple_bot.ts
@@ -13,22 +13,35 @@ import {
 
 
 
-function make_move(current: GameState): move {
+function make_move(current: GameState): move | null {
     const player = current.current_player;
-    let moves_left = current.dice;
-    while (moves_left !== []){
+    let moves_left = [...current.dice.values];
+    while (moves_left.length > 0){
+        let move_found = false;
         
-        for (let i = 23; i = 0; i--){
-            if (current.board.point[i] !== "black" ){
-                for (let y = 0; y < length.moves_left; y++){
-                    if (is_valid_move(current, point[i], moves_left[y]) === true){
-                        apply_move(current, point[i], moves_left[y])
-                        remove[moves_left[y], moves_left]
+        for (let i = 23; i >= 0; i--){
+            const point = current.board.points[i];
+
+            if (point.player === player){
+                for (let y = 0; y < moves_left.length; y++){
+                    
+
+                    if (is_valid_move(current, i, moves_left[y]) === true){
+                        apply_move(current, i, moves_left[y])
+                        moves_left.splice(y, 1);
+                        //remove[moves_left[y], moves_left] har inte någon fungerande remove funktion, lär finnas. bara för att gestalta. 
+                        move_found = true; 
+                        break; 
                     }
-                }
+                     
+                } 
+            if (move_found === true){
+                        break;
             }
-        }  
-    }
-    
-    
+            
+        }
+    } 
+    return null; 
 }
+    
+    
