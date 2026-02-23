ac82c6cce9eb9a26d2145c5d8d219e3a9568b2c3
AxelByback <axel.neckman-byback.5646@student.uu.se>
2026-02-23 12:44:36 +0100

    upd

--- game_logic/Game_board.ts
+++ game_logic/Game_board.ts
@@ -24,55 +24,55 @@ if (answer === "yes") {
 
 function play_game(state : GameState) : void {
 
-    while(game_over(state) === null) {
+    let move_count = 0;
 
-        let move_count = 0;
+    function check_move(state: GameState, from: number, die: number) : boolean {
+        if(is_valid_move(state, from, die)) {
+         apply_move(state, from, die)
+            move_count = move_count + 1;
+            if(move_count === state.dice?.values.length) {
+                switch_player(state);
+                move_count = 0;
+            }
+        }
+        return false;
+    }
+
+    function make_move(state: GameState) : void  {
+        if (state.dice?.values.length === 4) {
+           for ( let i = 0; i < 4; i++) {
+                console.log("Double dice! Nice");
+                const input_point = prompt("From what point do you make a move?");
+                const point = Number(input_point);
+                check_move(state, point, state.dice.values[i]);
+            }
+        }
+            
+        console.log("Make your move");
+        const input_move = prompt (`1. Make move with ${state.dice?.values[0]} \n
+                            2. Make move with ${state.dice?.values[1]}`);
+            
+        if (input_move === "1") {
+            const input_point = prompt("From what point do you make a move?")
+            const point = Number(input_point);
+            check_move(state, point, state.dice?.values[0]!)
+        } else if (input_move === "2") {
+            const input_point = prompt("From what point do you make a move?")
+            const point = Number(input_point);                
+            check_move(state, point, state.dice?.values[1]!)
+        } else {
+            console.log ("Wrong choice, try again")
+            make_move(state); 
+        }
+
+    }
+
+    while(game_over(state) === null) {
 
         if (state.dice === null) {
             state.dice = dice_roll();
-        }
-
-        function check_move(state: GameState, from: number, die: number) : boolean {
-            if(is_valid_move(state, from, die)) {
-                apply_move(state, from, die)
-                move_count = move_count + 1;
-                if(move_count === state.dice?.values.length) {
-                    switch_player(state);
-                    move_count = 0;
-                }
-            }
-            return false;
-        }
-    
+        }    
         
-        function make_move(state: GameState) : void  {
-            if (state.dice?.values.length === 4) {
-                for ( let i = 0; i < 4; i++) {
-                    console.log("Double dice! Nice");
-                    const input_point = prompt("From what point do you make a move?");
-                    const point = Number(input_point);
-                    check_move(state, point, state.dice.values[i]);
-                }
-            }
-            
-            console.log("Make your move");
-            const input_move = prompt (`1. Make move with ${state.dice?.values[0]} \n
-                    2. Make move with ${state.dice?.values[1]}`);
-            
-            if (input_move === "1") {
-                const input_point = prompt("From what point do you make a move?")
-                const point = Number(input_point);
-                check_move(state, point, state.dice?.values[0]!)
-            } else if (input_move === "2") {
-                const input_point = prompt("From what point do you make a move?")
-                const point = Number(input_point);
-                check_move(state, point, state.dice?.values[1]!)
-            } else {
-                console.log ("Wrong choice, try again")
-                make_move(state); 
-            }
-
-        }
         make_move(state);
 
     const winner = game_over(state);
