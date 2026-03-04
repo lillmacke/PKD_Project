cd89446484dc26d7423c2747f3cb1186ab111d06
lillmacke <markus9karlsson@hotmail.com>
2026-03-04 17:10:17 +0100

    asd

--- Backgammon bot/clone.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.clone = clone;
-function clone(state) {
-    //måste skrivas, ska klona state. verkar vara något som krävs för minmax
-    return JSON.parse(JSON.stringify(state));
-}
